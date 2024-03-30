f88caaa980ac9f42dfa7f3fd9f395431cdd50e33
safia-test <elmeslilis@gmal.com>
2024-03-30 10:53:36 +0000

    new commit to db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require(-'mongoose');
 
-mongoose.connect('mongodb://localhost/mydatabase', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect( 'mongodb://localhost/my_database', {
+useNewUrlParser: true,
+useUnifiedTopology: true
 })
-.then(() => {
-    console.log('MongoDB connected...');
-})
+
+.then(() => console.log( 'MongoDB connected ... ') )
 .catch(err => console.log(err));
