6dcc21aa64dd720a173edb29e0cf627da174b870
safia-test <elmeslilis@gmal.com>
2024-03-30 10:24:27 +0000

    commit methode get

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const User = require('./user');
 
 const app = express();
 app.use(express.json());
+
+// Get all users
+app.get('/users', async (req, res) => {
+    try {
+    const users = await User.find();
+    res. json(users);
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+    }});
