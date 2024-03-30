d7e1af3b334e0c58c28d60483a5ce952dbd92316
safia-test <elmeslilis@gmal.com>
2024-03-30 10:50:32 +0000

    commit --Create API Endpoints (get a single user by ID)

--- app.js
+++ app.js
@@ -79,3 +79,20 @@ async function getUser(req, res, next) {
 }
 
 app.listen(3000,() => console.log('Server started...'));
+
+// Middleware function to get a single user by ID
+async function getUser(req, res, next) {
+    let user;
+    try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+    return res.status(404).json({ message: 'Cannot find user' });
+    }
+    } catch (err) {
+    return res.status(500).json({ message:err.message });
+
+    }
+
+    res.user = user;
+    next();
+}
