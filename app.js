7dcf428eb99aa2d6220b5b32c08c20e612d44a8e
safia-test <elmeslilis@gmal.com>
2024-03-30 10:36:04 +0000

    commit methode post create new user

--- app.js
+++ app.js
@@ -17,3 +17,17 @@ app.get('/users', async (req, res) => {
 app.get('/users/:id', getUser, (req, res) => {
     res.json(res.user);
     });
+
+    app.post('/users', async (req, res) => {
+        const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+        });
+        
+        try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+        } catch (err) {
+        res.status(400).json({ message: err.message });
+        }});
